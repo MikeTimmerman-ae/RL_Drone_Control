cb2935a9f6557cb11cbcf6da63c25344b16a0117
Mike Timmerman <timmermanmike@hotmail.com>
2023-11-24 12:57:10 -0800

    implement evaluation metrics

---
+++ baseline/eval_performance.py
@@ -0,0 +1,15 @@
+""" File contains evaluation metrics to compare the performanc of different controller """
+
+import numpy as np
+
+
+def ISE(state_traj: np.ndarray, ref_traj: np.ndarray, dt: float) -> float:
+    ISE = np.sum(np.linalg.norm(state_traj[:, :2] - ref_traj[:-1, :2])) * dt
+    return ISE
+
+
+def ITSE(state_traj: np.ndarray, ref_traj: np.ndarray, dt: float) -> float:
+    ITSE = np.sum(np.array(
+        [i*dt * np.linalg.norm(state[:2] - ref[:2]) for i, state, ref in zip(list(range(len(state_traj))), state_traj, ref_traj)]
+    )) * dt
+    return ITSE
